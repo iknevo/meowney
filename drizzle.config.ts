d3d615c5c03a88eb81ef6a5f71f97c6ed366fee2
idknevo <nevoexists@gmail.com>
2025-08-06 18:02:54 +0300

    some small fixes

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "drizzle-kit";
 config({ path: ".env" });
 
 export default defineConfig({
-  out: "./drizzle",
+  out: "./src/drizzle",
   schema: "./src/db/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
--- src/db/scripts/migrate.ts
+++ src/db/scripts/migrate.ts
@@ -9,7 +9,7 @@ const sql = neon(process.env.DATABASE_URL!);
 const db = drizzle(sql);
 const main = async () => {
   try {
-    await migrate(db, { migrationsFolder: "drizzle" });
+    await migrate(db, { migrationsFolder: "src/drizzle" });
     console.log("Migration completed");
   } catch (error) {
     console.error("Error during migration:", error);
