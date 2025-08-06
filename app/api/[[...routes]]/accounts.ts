7d9e5a261405693af5e3278e06c4774030cc4bd6
idknevo <nevoexists@gmail.com>
2025-08-06 16:08:55 +0300

    api errors

--- app/api/[[...routes]]/accounts.ts
+++ app/api/[[...routes]]/accounts.ts
@@ -1,14 +1,25 @@
 import { db } from "@/db/dirzzle";
 import { accounts } from "@/db/schema";
+import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
+import { eq } from "drizzle-orm";
 import { Hono } from "hono";
 
-const app = new Hono().get("/", async (c) => {
+const app = new Hono().get("/", clerkMiddleware(), async (c) => {
+  const auth = getAuth(c);
+  if (!auth?.userId) {
+    // throw new HTTPException(401, {
+    //   res: c.json({ error: "Unauthorized" } as const, 401),
+    // });
+    return c.json({ error: "Unauthorized" } as const, 401);
+  }
   const data = await db
     .select({
       id: accounts.id,
       name: accounts.name,
     })
-    .from(accounts);
+    .from(accounts)
+    .where(eq(accounts.userId, auth.userId));
+
   return c.json({
     data,
   });
