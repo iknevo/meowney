b7fca8383a366621b674a4a52a8927fc635d5a73
idknevo <nevoexists@gmail.com>
2025-08-18 22:21:16 +0300

    fix: added dynamic

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,9 +1,23 @@
 import Dashboard from "@/src/features/dashboard/dashboard";
+import { Loader2 } from "lucide-react";
+import { Suspense } from "react";
+
+export const dynamic = "force-dynamic";
 
 export const metadata = {
   title: "Dashboard",
 };
 
 export default function DashboardPage() {
-  return <Dashboard />;
+  return (
+    <Suspense
+      fallback={
+        <div className="flex h-50 w-full items-center justify-center">
+          <Loader2 className="size-8 animate-spin text-slate-400" />
+        </div>
+      }
+    >
+      <Dashboard />
+    </Suspense>
+  );
 }
