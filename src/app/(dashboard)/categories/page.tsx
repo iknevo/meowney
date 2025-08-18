1e81ae179912007c3cbc26a27d40a70d7f327b35
idknevo <nevoexists@gmail.com>
2025-08-18 22:18:53 +0300

    fix: added dynamic

--- src/app/(dashboard)/categories/page.tsx
+++ src/app/(dashboard)/categories/page.tsx
@@ -1,9 +1,22 @@
 import Categories from "@/src/features/categories/components/categories";
+import { Loader2 } from "lucide-react";
+import { Suspense } from "react";
 
+export const dynamic = "force-dynamic";
 export const metadata = {
   title: "Categories",
 };
 
 export default function Page() {
-  return <Categories />;
+  return (
+    <Suspense
+      fallback={
+        <div className="flex h-50 w-full items-center justify-center">
+          <Loader2 className="size-8 animate-spin text-slate-400" />
+        </div>
+      }
+    >
+      <Categories />;
+    </Suspense>
+  );
 }
--- src/app/(dashboard)/transactions/page.tsx
+++ src/app/(dashboard)/transactions/page.tsx
@@ -1,9 +1,22 @@
 import Transactions from "@/src/features/transactions/components/transactions";
+import { Loader2 } from "lucide-react";
+import { Suspense } from "react";
 
+export const dynamic = "force-dynamic";
 export const metadata = {
   title: "Transactions",
 };
 
 export default function Page() {
-  return <Transactions />;
+  return (
+    <Suspense
+      fallback={
+        <div className="flex h-50 w-full items-center justify-center">
+          <Loader2 className="size-8 animate-spin text-slate-400" />
+        </div>
+      }
+    >
+      <Transactions />;
+    </Suspense>
+  );
 }
